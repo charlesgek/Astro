cbe5eb2ebf433a45f0bbc7a68f109b6f7318317e
charlesgek <contact@charlesgek.com>
2025-03-20 17:56:28 +0000

    update 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite";
+import solidJs from "@astrojs/solid-js"
+
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
       },
+    site: "http://localhost:4321",
+    integrations: [mdx(), sitemap(), solidJs()],
 });
