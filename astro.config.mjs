31602ef5817302336ca1afef96bcc0c64c1ecbe2
charlesgek <contact@charlesgek.com>
2025-03-22 23:20:22 +0000

    Added Turnstile

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,11 @@ import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js"
 
-
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-      },
-    site: "http://localhost:4321",
-    integrations: [mdx(), sitemap(), solidJs()],
+  vite: {
+      plugins: [tailwindcss()],
+    },
+  site: "http://localhost",
+  integrations: [mdx(), sitemap(), solidJs()],
 });
