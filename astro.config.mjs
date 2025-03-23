2318e3f0bdf72f26d19a09447997b053111ce605
charlesgek <contact@charlesgek.com>
2025-03-23 19:23:18 +0000

    Cloudflare + Wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,20 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js"
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
     },
-  site: "http://localhost",
+  site: "http://localhost:4321",
   integrations: [mdx(), sitemap(), solidJs()],
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.toml",
+    },
+  }),
 });
