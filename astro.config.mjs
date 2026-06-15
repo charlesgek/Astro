d459d42aae9c5c5fe9b9180a2a691743096864fc
charlesgek <contact@charlesgek.com>
2026-06-15 13:28:37 +0800

    esbuild solidjs compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,21 @@ import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js"
 import cloudflare from "@astrojs/cloudflare";
+import { solidPlugin } from 'esbuild-plugin-solid';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [solidPlugin()],
+        target: 'es2022'
+      }
+    },
+    build: {
+      target: 'es2022'
+    }
   },
   site: "https://charlesgek.com",
   integrations: [mdx(), sitemap(), solidJs()],
